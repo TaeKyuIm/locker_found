522f1e085410454999612b37f531bfcfbedfc2d5
임태규 <kyu5787@naver.com>
2022-08-19 18:54:03 +0900

    secrets json delete

--- server/conf/urls.py
+++ server/conf/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     path('users/', include('users.urls', namespace='users')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include('locker.urls')),
 ]
 
 if settings.DEBUG:
